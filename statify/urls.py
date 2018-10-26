99e3580bc43eb0c65c5ea969c6d506d2b5918097
Xander Riga <xanderriga@live.com>
2018-10-26 01:32:34 -0700

    did basic auth code flow, but package seems to not work at all
    My code should work based on teh spec, but the package hasnt been
    updated in about a year, so I will have to modify the package itself

--- statify/urls.py
+++ statify/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'statify'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:user_id>', views.detail, name='detail')
+    path('<int:user_id>', views.detail, name='detail'),
+    path('spotifyauth', views.spotifyauth, name='spotifyauth'),
+    path('callback', views.callback, name='callback')
 ]
--- statify/views.py
+++ statify/views.py
@@ -1,7 +1,15 @@
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
 
 from .models import User
+import spotipy
+import spotipy.util as util
+
+SPOTIPY_CLIENT_ID = 'ff706cadbcd2445782b2c3719bace226'
+SPOTIPY_CLIENT_SECRET = '538dc4b9fd7e4da48df760c4a99f2e1d'
+SPOTIPY_REDIRECT_URI = 'http://localhost:8000/statify/callback'
+SCOPE = 'user-library-read'
+CACHE = '.spotipyoauthcache'
 
 
 def index(request):
@@ -16,3 +24,25 @@ def detail(request, user_id):
         raise Http404('User does not exist')
 
     return render(request, 'statify/detail.html', {'user': user})
+
+
+def spotifyauth(request):
+    username = 'xanderdagr8'
+    token = util.prompt_for_user_token(username, scope=SCOPE, client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI)
+
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        playlists = sp.user_playlists(username)
+        while playlists:
+            for i, playlist in enumerate(playlists['items']):
+                print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'], playlist['name']))
+            if playlists['next']:
+                playlists = sp.next(playlists)
+            else:
+                playlists = None
+    return
+    # return render(request, 'statify/savedtracks.html', results)
+
+
+def callback(request):
+    return HttpResponse('Spotify successfully logged in. You can close this window and return to your previous tab')
